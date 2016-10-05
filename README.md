7099ab2541253f2b9c08aa0715e160272bf695b1
ee2dev <ee2.dev@gmail.com>
2016-10-06 00:00:26 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,82 @@
+# sequence explorer
+Adapting the sankey diagram for sequential data. There are many applications for exploring the development of categories over some sequence (e.g. time points such as days, months or years).
+Sequence explorer implements the following [d3.js reusable charts pattern](https://github.com/EE2dev/d3-template) to let you customize the chart.
+
+<!--
+[Here](https://youtu.be/B8a2O6L31_w) is a link to a video explaining how to use item explorer with your own data and [here](http://www.ankerst.de/Mihael/proj/mbc/) is a web site introducing item explorer.
+--->
+
+### To do
+
+- change initialization of single/ multiples
+- change <pre> into divs, also if <pre>
+- change conditions dimCol == "" /dimRow into allGraphs.cols/ .rows === 1
+- tooltip
+- add names for x and y axes for tooltip
+- multiplesColumns(boolean)
+- css styling
+- node text an/aus
+- testing browsers (change display:table)
+- remove test layout in css
+- licence
+
+### Data
+
+- [demography](http://www.bib-demografie.de/DE/ZahlenundFakten/02/Tabellen/t_02_01_bevstand_d_1960_2060.html;jsessionid=F996B8093DC563B8B2A5F791C5683174.2_cid292?nn=3074120)
+-----------------------
+
+### 1. Data formatting
+
+The file must contain each pair of sequential categories with the corresponding quantity.
+The file must be a comma separated file with the first row containing the attribute names.
+
+The order of the columns must be:
+* first column: the quantity to be displayed 
+* second column: the first category 
+* third column: the state of the sequence corresponding to the first category
+* fourth column: the second category (according to the sequence) 
+* fifth column: the state of the sequence corresponding to the second category
+
+Example of valid csv files:
+```
+frequency,mood_from,day_from,mood_to,day_to
+50,good,Monday,good,Tuesday
+60,good,Monday,ok,Tuesday
+20,good,Monday,bad,Tuesday
+30,ok,Tuesday,good,Wednesday
+...
+```
+or
+```
+_value,cat1,state1,cat2,state2
+2650,fruit,1,fruit,2
+7860,candy,1,fruit,2
+3450,meat,2,fruit,3
+5430,fish,2,fish,3
+...
+```
+or
+```
+quantity,grade_previous,year1,grade_next,year2
+10,A,2010,A,2011
+6,A,2010,B,2011
+20,A,2011,A,2012
+14,B,2011,B,2012
+...
+```
+
+####Pivot by additional categories
+
+### 3. Visualization options
+function | parameter | explanation
+------------ | -------------
+`debugOn()` | *boolean* | e.g. `sankeySeq.debug(true)` turns on/off the console.log debugging. The default setting is false.
+`size()` | *2-dim array* |, e.g. `sankeySeq.size([600, 400])` explicitely sets size of the SVG based on an array [width, height]. The default size is [700, 500].
+`margin()` | *integer* | e.g. `sankeySeq.margin(10)` explicitely sets margin in pixels for top, right, bottom, left. The default margin is 0 px.
+`sequence()` | *array* | e.g. `sankeySeq.sequence(["2000", "2001", "2002"])` explicitely sets the order of the sequence based on an array. The default order is ascending.
+`categories()` | *array* | e.g. `sankeySeq.categories(["A", "B", "C"])` explicitely sets the order of the categories based on an array. The default order is ascending.
+`sequenceName()` | *string* | e.g. `sankeySeq.sequenceName("year")` explicitely sets the name of the x axis. The default name is "sequence".
+`categoryName()` | *string* | e.g. `sankeySeq.categoryName("state")` explicitely sets the name of the y axis. The default name is "category".
+`valueName()` | *string* | e.g. `sankeySeq.valueName("frequency")` explicitely sets the name of the value. The default name is "value".
+`nodeWidth()` | *integer* | e.g. `sankeySeq.nodeWidth(20)` explicitely sets the width of a node in pixels. The default width is 15.
+`nodePadding()` | *integer* | e.g. `sankeySeq.nodePadding(10)` explicitely sets the y-padding between the categories in pixels. The default padding is 8.
---
+++ sankeySeq.js
@@ -0,0 +1,215 @@
+﻿ // extension of sankey based on Andrey's reply to http://stackoverflow.com/questions/21539265/d3-sankey-charts-manually-position-node-along-x-axis
+d3.sankeySeq = function() {
+  var sankey  = {},
+	  debugOn = false,
+		nodeWidth = 15,
+		nodePadding = 8, 
+		size = [700, 500],
+	  sequence = [],
+	  categories = [],
+		ky, // scaling factor for height of node
+		nodes = [],
+		links = [],
+	  xScale,
+	  yScale;
+ 
+  sankey.debugOn = function(_) {
+    if (!arguments.length) return debugOn;
+    debugOn = _;
+    return sankey;
+  };
+
+  sankey.nodeWidth = function(_) {
+    if (!arguments.length) return nodeWidth;
+    nodeWidth = +_;
+    return sankey;
+  };
+  
+  sankey.nodePadding = function(_) { 
+    if (!arguments.length) return nodePadding;
+    nodePadding = +_;
+    return sankey;
+  };
+ 
+  sankey.nodes = function(_) {
+    if (!arguments.length) return nodes;
+    nodes = _;
+    return sankey;
+  };
+ 
+  sankey.links = function(_) {
+    if (!arguments.length) return links;
+    links = _;
+    return sankey;
+  };
+  
+  sankey.sequence = function(_) {
+    if (!arguments.length) return sequence;
+    sequence = _;
+    return sankey;
+  };
+  
+  sankey.categories = function(_) {
+    if (!arguments.length) return categories;
+    categories = _;
+    return sankey;
+  };
+ 
+  sankey.size = function(_) {
+    if (!arguments.length) return size;
+    size = _;
+    return sankey;
+  };
+  
+  sankey.xScale = function(_) {
+    if (!arguments.length) return xScale;
+    xScale = _;
+    return sankey;
+  };
+  
+  sankey.yScale = function(_) {
+    if (!arguments.length) return yScale;
+    yScale = _;
+    return sankey;
+  };
+  
+  sankey.relayout = function() {
+    computeLinkDepths();
+    return sankey;
+  };
+ 
+  sankey.link = function() {
+    var curvature = .5;
+ 
+    function link(d) {
+      var x0 = d.source.x + d.source.dx,
+          x1 = d.target.x,
+          xi = d3.interpolateNumber(x0, x1),
+          x2 = xi(curvature),
+          x3 = xi(1 - curvature),
+          y0 = d.source.y + d.sy + d.dy / 2,
+          y1 = d.target.y + d.ty + d.dy / 2;
+      return "M" + x0 + "," + y0
+           + "C" + x2 + "," + y0
+           + " " + x3 + "," + y1
+           + " " + x1 + "," + y1;
+    }
+ 
+    link.curvature = function(_) {
+      if (!arguments.length) return curvature;
+      curvature = +_;
+      return link;
+    };
+ 
+    return link;
+  };
+ 
+  sankey.layout = function() { 
+    computeNodeLinks();
+    computeNodeValues();
+    computeNodeSizes(); // new
+    computeNodePositions(); // new
+    computeLinkDepths();
+    return sankey;
+  };
+	
+	sankey.getNodeHeight = function(value) {
+		return value * ky;
+	};
+  // end of API functions
+  
+  // Populate the sourceLinks and targetLinks for each node.
+  // Also, if the source and target are not objects, assume they are indices.
+  function computeNodeLinks() {
+    nodes.forEach(function(node) {
+      node.sourceLinks = [];
+      node.targetLinks = [];
+    });
+    links.forEach(function(link) {
+      var source = link.source,
+          target = link.target;
+      if (typeof source === "number") source = link.source = nodes[link.source];
+      if (typeof target === "number") target = link.target = nodes[link.target];
+      source.sourceLinks.push(link);
+      target.targetLinks.push(link);
+    });
+  }
+ 
+  // Compute the value (size) of each node by summing the associated links.
+  function computeNodeValues() {
+    nodes.forEach(function(node) {
+      node.value = Math.max(
+        d3.sum(node.sourceLinks, value),
+        d3.sum(node.targetLinks, value)
+      );
+    });
+  }
+
+  // Compute the y extend of nodes and links
+  function computeNodeSizes() {
+	// calculate scaling factor ky based on #categories, height, nodePAdding and maxNodeValue
+	ky = ((size[1] / categories.length) - nodePadding) 
+		/ d3.max(nodes, function (d) { return d.value;});
+    
+	if (debugOn) {console.log("ky: " + ky);}
+	
+    nodes.forEach(function(node, i) {
+      node.dy = node.value * ky;
+      node.dx = nodeWidth;
+    });
+
+    links.forEach(function(link) {
+      link.dy = link.value * ky;
+    });
+  }  
+  
+  function computeNodePositions() {
+	  console.log(size);
+	  xScale = d3.scalePoint().domain(sequence).range([0, size[0] - nodeWidth]);    
+    yScale = d3.scalePoint().domain(categories).range([size[1], (size[1] / categories.length)]); 	
+    
+    nodes.forEach(function(element) {
+      element.x = xScale(element.nameX);
+      element.y = yScale(element.nameY) - element.dy;
+			
+			if (debugOn) {
+				console.log("x " + element.nameX + " -> " + xScale(element.nameX));
+				console.log("y " + element.nameY + " -> " + xScale(element.nameY));
+			}
+    });  
+
+    if (debugOn) {console.log(nodes);}
+  };
+  
+  function computeLinkDepths() {
+    nodes.forEach(function(node) {
+      node.sourceLinks.sort(ascendingTargetDepth);
+      node.targetLinks.sort(ascendingSourceDepth);
+    });
+    nodes.forEach(function(node) {
+      var sy = 0, ty = 0;
+      node.sourceLinks.forEach(function(link) {
+        link.sy = sy;
+        sy += link.dy;
+      });
+      node.targetLinks.forEach(function(link) {
+        link.ty = ty;
+        ty += link.dy;
+      });
+    });
+ 
+    function ascendingSourceDepth(a, b) {
+      return a.source.y - b.source.y;
+    }
+ 
+    function ascendingTargetDepth(a, b) {
+      return a.target.y - b.target.y;
+    }
+  }
+ 
+  function value(link) {
+    return link.value;
+  }
+  
+  return sankey;
+};
