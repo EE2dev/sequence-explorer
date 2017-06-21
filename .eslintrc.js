196f883a1c6721c9f1ab98b9f3c7eeffb646709f
ee2dev <ee2.dev@gmail.com>
2017-06-22 00:41:00 +0200

    files added

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            2
+        ],
+        "linebreak-style": [
+            "warn",
+            "unix"
+        ],
+        "quotes": [
+            "warn",
+            "double"
+        ],
+        "no-console": 0,
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
